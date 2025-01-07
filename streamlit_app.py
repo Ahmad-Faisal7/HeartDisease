5069c26f5e6abd20333a1a7d021bd8e70d925133
Ahmad-Faisal7 <jstahmad@gmail.com>
2025-01-07 21:56:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Streamlit Application Title
 st.title("🫀🩺 Heart Disease Application")
